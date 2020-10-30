1d01fa38042530b2cdce3fa374db6b03978166c6
Ian Panlilio <ipanlilio7@gmail.com>
2020-10-30 00:01:49 -0700

    refined if and else statements, added functionalitty for adding multiple employees

--- Develop/app.js
+++ Develop/app.js
@@ -9,80 +9,106 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const employeelist = [];
 
-inquirer
+function questions() {
+  inquirer
     .prompt([
-        {
-            type: 'list',
-            name: 'role',
-            message: "Select Role",
-            choices: [ 'Manager', 'Engineer', 'Intern'],
-
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: "Input email",
-            validate: noBlank
-
-        },
-        {
-            type: 'input',
-            name: 'name',
-            message: "Input Username",
-            validate: noBlank
-
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: "Create a User ID",
-            validate: noBlank
-
-        },
-        {
-            type: 'input',
-            name: 'officeNumber',
-            message: "What is the manager's office number?",
-            when: (answers) => answers.role === 'Manager',
-            validate: noBlank
-        },
-        {
-            type: 'input',
-            name: 'github',
-            message: "What is the Engineer's GitHub username?",
-            when: (answers) => answers.role === 'Engineer',
-            validate: noBlank
-        },
-        {
-            type: 'input',
-            name: 'school',
-            message: "Which school is the intern from?",
-            when: (answers) => answers.role === 'Intern',
-            validate: noBlank
-        }
+      {
+        type: "list",
+        name: "role",
+        message: "Select Role",
+        choices: ["Manager", "Engineer", "Intern"],
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Input email",
+        validate: noBlank,
+      },
+      {
+        type: "input",
+        name: "name",
+        message: "Input Username",
+        validate: noBlank,
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Create a User ID",
+        validate: noBlank,
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the manager's office number?",
+        when: (answers) => answers.role === "Manager",
+        validate: noBlank,
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What is the Engineer's GitHub username?",
+        when: (answers) => answers.role === "Engineer",
+        validate: noBlank,
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "Which school is the intern from?",
+        when: (answers) => answers.role === "Intern",
+        validate: noBlank,
+      },
     ])
-    .then(answers => {
-
-        if (answers.role === "Manager" ){
-            const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber)
-        }
-        else if (answers.role === "Intern"){
-            const intern = new Intern(answers.name, answers.id, answers.email, answers.school)
-        }
-        else if (answers.role === "Engineer"){
-            const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github)
-        }
-
-
-    })
-
-
-    function noBlank(input) {
-        return input !== '' || "Cannot leave blank";
+    .then((answers) => {
+      if (answers.role === "Manager") {
+        const manager = new Manager(
+          answers.name,
+          answers.id,
+          answers.email,
+          answers.officeNumber
+        );
+        employeelist.push(manager);
+      } else if (answers.role === "Intern") {
+        const intern = new Intern(
+          answers.name,
+          answers.id,
+          answers.email,
+          answers.school
+        );
+        employeelist.push(intern);
+      } else if (answers.role === "Engineer") {
+        const engineer = new Engineer(
+          answers.name,
+          answers.id,
+          answers.email,
+          answers.github
+        );
+        employeelist.push(engineer);
       }
-      
+      inquirer
+        .prompt({
+          type: "confirm",
+          name: "confirm",
+          message: "Add another team member?",
+        })
+        .then((answers) => {
+          if (answers.confirm) {
+            questions();
+          } else {
+            fs.writeFile("team.html", render(employeelist), (err) =>
+              console.log(err)
+            );
+          }
+        });
+    });
+}
 
+function noBlank(input) {
+  return input !== "" || "Cannot leave blank";
+}
+
+questions();
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
